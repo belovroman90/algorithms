9a0b46735d40e0df01865b67dab93c9de7345133
belovroman90 <belov.roman.90@mail.ru>
2022-08-02 08:17:09 +0300

    Realize second version of selection sort function

--- sort/selection-sort.js
+++ sort/selection-sort.js
@@ -1,6 +1,6 @@
 const someArray = [4, 1, 5, 2, 3, 0, -1]
-
-const selectSort = array => {
+// v1
+const selectionSort = array => {
     const arr = [...array]
 
     for (let i = 0; i < arr.length - 1; i++) {
@@ -20,4 +20,35 @@ const selectSort = array => {
     return arr
 }
 
-console.log(selectSort(someArray))
+console.log(selectionSort(someArray))
+
+// v2
+function smallest(array) {
+    let indexOfMin = 0
+    for (let i = 0; i <= array.length; i++) {
+        if (array.length === 1) {
+            indexOfMin = i
+        } else if (array[i] < array[indexOfMin]) {
+            indexOfMin = i
+        }
+    }
+    return indexOfMin
+}
+
+function selectionSort2(array) {
+    const copyArr = [...array]
+    const result = []
+
+    while (copyArr.length) {
+        let indexOfMin = smallest(copyArr);
+        if (copyArr.length === 1) result.push(copyArr.shift())
+        if (copyArr[0] >= copyArr[indexOfMin]) {
+            [copyArr[0], copyArr[indexOfMin]] = [copyArr[indexOfMin], copyArr[0]]
+            result.push(copyArr.shift())
+        }
+    }
+
+    return result
+}
+
+console.log(selectionSort2(someArray))
